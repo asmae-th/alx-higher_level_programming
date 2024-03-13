7ac54e4f5fc0b17c68046b21c44fd3afb697039c
Asmae Tahtouh <atahtouh@e2r5p5.1337.ma>
2024-03-13 23:14:03 +0000

    done

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
 i = 97
-output = ""
 while i <= 122:
-    if i != 101 and i != 113:
-        output += chr(i)
+    if i != 101 and i != 113 :
+        print("{}".format(chr(i)), end="")
     i += 1
-
-print(output)
